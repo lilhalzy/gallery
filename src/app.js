4c73cae654d1e873df066b983ba3dd9152f1480c
lilhalzy <lilhalzy11@gmail.com>
2022-09-25 10:36:19 +0800

    refactor: refactor to create a separate component for the gallery

--- src/app.js
+++ src/app.js
@@ -1,50 +1,5 @@
-import Gallery from 'react-photo-gallery';
-const gallery = require("./gallery.json")
+import { Gallery } from "./gallery";
 
 export function App() {
-
-  const rows = []
-
-  let currRow = {
-    items: []
-  }
-  rows.push(currRow)
-
-  for (const item of gallery) {
-    currRow.items.push(item)
-  }
-
-  const photoWidth = 600
-
-  return (
-    <div
-      style={{
-        width: `${photoWidth}px`,
-        border: "solid red 3px"
-      }}
-      >
-      {rows.map((row, rowIndex) => {
-        return (
-          <div
-            key={rowIndex}
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              height: `200px`
-            }}
-            >
-            {row.items.map(item => {
-              return (
-                <img
-                  key={item.urls.thumb}
-                  src={item.urls.thumb}
-                  />
-              )
-            })}
-          </div>
-          )
-      })}
-
-      </div>
-  )
+  return <Gallery />;
 }
---
+++ src/gallery.js
@@ -0,0 +1,42 @@
+const gallery = require("./gallery.json");
+
+export function Gallery() {
+  const rows = [];
+
+  let currRow = {
+    items: [],
+  };
+  rows.push(currRow);
+
+  for (const item of gallery) {
+    currRow.items.push(item);
+  }
+
+  const photoWidth = 600;
+
+  return (
+    <div
+      style={{
+        width: `${photoWidth}px`,
+        border: "solid red 3px",
+      }}
+    >
+      {rows.map((row, rowIndex) => {
+        return (
+          <div
+            key={rowIndex}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              height: `200px`,
+            }}
+          >
+            {row.items.map((item) => {
+              return <img key={item.urls.thumb} src={item.urls.thumb} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
