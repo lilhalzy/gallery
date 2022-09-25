1e9541c40477dbe89e9861c4da0715022e6dc463
lilhalzy <lilhalzy11@gmail.com>
2022-09-25 10:31:37 +0800

    feat: simple gallery row

--- src/app.js
+++ src/app.js
@@ -1,17 +1,50 @@
-let gallery = require("./gallery.json")
-
-gallery = gallery.map(item => {
-  return {
-    src: item.urls.thumb,
-    width: item.width,
-    height: item.height
-  }
-})
-
-console.log(gallery);
+import Gallery from 'react-photo-gallery';
+const gallery = require("./gallery.json")
 
 export function App() {
+
+  const rows = []
+
+  let currRow = {
+    items: []
+  }
+  rows.push(currRow)
+
+  for (const item of gallery) {
+    currRow.items.push(item)
+  }
+
+  const photoWidth = 600
+
   return (
-    <h1>Sup peopeww</h1>
+    <div
+      style={{
+        width: `${photoWidth}px`,
+        border: "solid red 3px"
+      }}
+      >
+      {rows.map((row, rowIndex) => {
+        return (
+          <div
+            key={rowIndex}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              height: `200px`
+            }}
+            >
+            {row.items.map(item => {
+              return (
+                <img
+                  key={item.urls.thumb}
+                  src={item.urls.thumb}
+                  />
+              )
+            })}
+          </div>
+          )
+      })}
+
+      </div>
   )
 }
