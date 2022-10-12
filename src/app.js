49097a786d6d060eaaf8cfc73baffc5423185993
lilhalzy <lilhalzy11@gmail.com>
2022-10-12 11:29:27 +0800

    refactor: pass values through gallery components as props

--- src/app.js
+++ src/app.js
@@ -1,5 +1,45 @@
 import { Gallery } from "./gallery";
 
+
+let gallery = require('./gallery.json')
+
+gallery = gallery.map(item => {
+  return{
+    thumb: item.urls.thumb,
+    width: item.width,
+    height: item.height,
+  }
+})
+
+// const gallery = [
+//     {
+//       thumb: "https://via.placeholder.com/140x100",
+//       width: 140,
+//       height: 100,
+//     }
+// ]
+
+// const gallery = [
+//     {
+//       thumb: "https://via.placeholder.com/240x200",
+//       width: 240,
+//       height: 200,
+//     },
+//     {
+//       thumb: "https://via.placeholder.com/220x200",
+//       width: 220,
+//       height: 200,
+//     },
+//     {
+//       thumb: "https://via.placeholder.com/230x200",
+//       width: 230,
+//       height: 200,
+//     },
+// ];
 export function App() {
-  return <Gallery />;
+  return <Gallery 
+    photoWidth={600}
+    targetRowHeight={200}
+    items={gallery}
+    />;
 }
--- src/gallery.js
+++ src/gallery.js
@@ -1,46 +1,8 @@
 import { createLayout } from "./layout";
 
-let gallery = require('./gallery.json')
+export function Gallery({ items, photoWidth, targetRowHeight}) {
 
-gallery = gallery.map(item => {
-  return{
-    thumb: item.urls.thumb,
-    width: item.width,
-    height: item.height,
-  }
-})
-
-// const gallery = [
-//     {
-//       thumb: "https://via.placeholder.com/140x100",
-//       width: 140,
-//       height: 100,
-//     }
-// ]
-
-// const gallery = [
-//     {
-//       thumb: "https://via.placeholder.com/240x200",
-//       width: 240,
-//       height: 200,
-//     },
-//     {
-//       thumb: "https://via.placeholder.com/220x200",
-//       width: 220,
-//       height: 200,
-//     },
-//     {
-//       thumb: "https://via.placeholder.com/230x200",
-//       width: 230,
-//       height: 200,
-//     },
-// ];
-
-export function Gallery() {
-
-  const photoWidth = 600;
-  const targetRowHeight = 200
-  const rows = createLayout(gallery, photoWidth, targetRowHeight)
+  const rows = createLayout(items, photoWidth, targetRowHeight)
 
   return (
     <div
